836c01ff6ded8799328238ceea060606b2737f86
Lore0905 <lore.castelli4@gmail.com>
2021-12-02 15:51:50 +0100

    done

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ Vue.config.devtools = true;
 
 // Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-// BONUS:
-// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
 
 const titleText = new Vue(
     {
